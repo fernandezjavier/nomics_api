39073e8597a541271b35f68ab71a50aa900f1a1d
Javier Fernandez <fernandezjavierok@gmail.com>
2022-04-05 08:17:20 +0100

    Task two spec

--- specs.rb
+++ specs.rb
@@ -27,3 +27,36 @@ RSpec.describe 'TaskOne' do
     expect(result.dig(1, 'id')).to eq('AXS')
   end
 end
+
+RSpec.describe 'TaskTwo' do
+  let(:task) { TaskTwo.new(['BTT', 'AXS'], ['id', 'status']) }
+  let(:result) { task.run }
+
+  before do
+    allow(NomicsClient).to receive(:new).and_return(double(get: [
+      {
+        'id' => 'BTT',
+        'status' => 'active',
+        'price' => '0.0041642670'
+      },
+      {
+        'id' => 'AXS',
+        'status' => 'dead',
+        'price' => '0.000069194533'
+      }
+    ]))
+  end
+
+  it 'returns selected payload, given tickers' do
+    expect(result).to eq([
+      {
+        'id' => 'BTT',
+        'status' => 'active'
+      },
+      {
+        'id' => 'AXS',
+        'status' => 'dead'
+      }
+    ])
+  end
+end
