1fc06b59a546f842001e3450deee89ad23828f5f
Javier Fernandez <fernandezjavierok@gmail.com>
2022-04-05 10:29:24 +0100

    Task three

--- tasks.rb
+++ tasks.rb
@@ -29,3 +29,25 @@ class TaskTwo
       .map { |crypto| crypto.slice(*fields) }
   end
 end
+
+class TaskThree
+  attr_reader :currency, :reference_fiat
+
+  def initialize(currency:, reference_fiat:)
+    @currency = currency
+    @reference_fiat = reference_fiat
+  end
+
+  def run
+    query_params = {
+      ids: currency,
+      'quote-currency': reference_fiat
+    }
+
+    NomicsClient
+      .new('currencies/ticker', query_params)
+      .get
+      .dig(0, 'price')
+      .to_f
+  end
+end
