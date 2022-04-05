82dd56d17483e9d217de47b17622cb7abd914751
Javier Fernandez <fernandezjavierok@gmail.com>
2022-04-05 10:50:06 +0100

    Task four

--- tasks.rb
+++ tasks.rb
@@ -51,3 +51,28 @@ class TaskThree
       .to_f
   end
 end
+
+class TaskFour
+  attr_reader :currency, :reference_crypto
+
+  def initialize(currency:, reference_crypto: 'BTC')
+    @currency = currency
+    @reference_crypto = reference_crypto
+  end
+
+  def run
+    pair = NomicsClient.new('currencies/ticker', ids: ids).get
+
+    price(pair.first) / price(pair.last)
+  end
+
+  private
+
+  def ids
+    [currency, reference_crypto].join(',')
+  end
+
+  def price(crypto)
+    crypto.fetch('price').to_f
+  end
+end
