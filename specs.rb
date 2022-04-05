a0435f8d107996b518ac11a378efbfd5f21ebc93
Javier Fernandez <fernandezjavierok@gmail.com>
2022-04-05 08:25:46 +0100

    Task three spec

--- specs.rb
+++ specs.rb
@@ -60,3 +60,22 @@ RSpec.describe 'TaskTwo' do
     ])
   end
 end
+
+RSpec.describe 'TaskThree' do
+  let(:task) { TaskThree.new(currency: 'BTT', reference_fiat: 'EUR') }
+  let(:result) { task.run }
+
+  before do
+    allow(NomicsClient).to receive(:new).and_return(double(get: [
+      {
+        'id' => 'BTT',
+        'status' => 'active',
+        'price' => '0.0041642670'
+      }
+    ]))
+  end
+
+  it 'returns selected payload, given tickers' do
+    expect(result).to eq(0.0041642670)
+  end
+end
