494ee3060b3376f470e73f9a5a100ad4204e5ec4
Javier Fernandez <fernandezjavierok@gmail.com>
2022-04-05 10:21:22 +0100

    Task two

--- tasks.rb
+++ tasks.rb
@@ -13,3 +13,19 @@ class TaskOne
       .get
   end
 end
+
+class TaskTwo
+  attr_reader :tickers, :fields
+
+  def initialize(tickers = [], fields = [])
+    @tickers = tickers
+    @fields = fields
+  end
+
+  def run
+    NomicsClient
+      .new('currencies/ticker', ids: tickers.join(','))
+      .get
+      .map { |crypto| crypto.slice(*fields) }
+  end
+end
